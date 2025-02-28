8556b9e176418d32857cd884b7b83ba11db96b7d
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-02-28 20:22:57 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>amongus</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import styleX from "vite-plugin-stylex";
+import { ngrok } from "vite-plugin-ngrok";
+
+export default defineConfig({
+  plugins: [react(), styleX(), ngrok("")],
+});
