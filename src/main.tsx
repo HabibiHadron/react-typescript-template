ed3697dd9fd0bac056f1cfe88b4e92c328ed77d1
Abdullah Zafar <abdullahzafarrana@gmail.com>
2025-03-01 03:26:27 +0500

    added redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,21 @@ import "./stylex.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </QueryClientProvider>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import styleX from "vite-plugin-stylex";
 import { ngrok } from "vite-plugin-ngrok";
 
 export default defineConfig({
-  plugins: [react(), styleX(), ngrok("")],
+  plugins: [react(), styleX(), ngrok("ENTER YOUR TOKEN HERE")],
 });
